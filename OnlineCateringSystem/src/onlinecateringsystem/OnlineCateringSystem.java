3bb87feaa26baf9b5c57794a7ce3d931e03f7b25
KaoWei1116 <76992701+KaoWei1116@users.noreply.github.com>
2023-03-15 22:01:32 +0800

    Login Done

---
+++ OnlineCateringSystem/src/onlinecateringsystem/OnlineCateringSystem.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onlinecateringsystem;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class OnlineCateringSystem {
+
+    public static String finalUsername = null;
+    static Scanner scanner = new Scanner(System.in);
+
+    private static void startingInterfaceMenu() throws IOException, FileNotFoundException, InterruptedException {
+        int choice = 0;
+        Login login = new Login();
+        do {
+            System.out.print("\n");
+            System.out.printf(" _____________________________________________\n");
+            System.out.printf("[       Welcome to FourFriends Catering!      ]\n");
+            System.out.printf("]   Where customer satisfaction comes first   [\n");
+            System.out.printf("[_____________________________________________]\n");
+            System.out.println("=============================================");
+            System.out.println("|   1. Login to an existing account         |");
+            System.out.println("|   2. Register to create a new account     |");
+            System.out.println("|   3. Exit program                         |");
+            System.out.println("|   4. Enter 4 if you forget password       |");
+            System.out.println("=============================================");
+            System.out.print("\n");
+            System.out.print("Enter an number : ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("\n");
+                    System.out.println("=====================================");
+                    System.out.println("|            Login Page             |");
+                    System.out.println("=====================================");
+                    login.loginPage();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.exit(0);
+                case 4:
+                    break;
+                default:
+                    System.out.println("Please insert an integer between 1 to 4. Thank you.");
+                    break;
+            }
+        } while (choice < 1 || choice > 4);
+    }
+
+    public static void main(String[] args) throws IOException, FileNotFoundException, InterruptedException {
+        startingInterfaceMenu();
+    }
+
+}
