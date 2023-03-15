29c90748d3eb54fb8a50e3770fd949dd056f2530
KaoWei1116 <76992701+KaoWei1116@users.noreply.github.com>
2023-03-15 22:46:33 +0800

    Login & Logout Done

--- OnlineCateringSystem/src/onlinecateringsystem/OnlineCateringSystem.java
+++ OnlineCateringSystem/src/onlinecateringsystem/OnlineCateringSystem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package onlinecateringsystem;
 
 import java.io.FileNotFoundException;
@@ -11,14 +6,15 @@ import java.util.Scanner;
 
 /**
  *
- * @author User
+ * @author Chong Kao Wei
  */
 public class OnlineCateringSystem {
 
     public static String finalUsername = null;
     static Scanner scanner = new Scanner(System.in);
 
-    private static void startingInterfaceMenu() throws IOException, FileNotFoundException, InterruptedException {
+    private static String startingInterfaceMenu() throws IOException, FileNotFoundException, InterruptedException {
+        String usernameInputCheck = "exit";
         int choice = 0;
         Login login = new Login();
         do {
@@ -42,9 +38,27 @@ public class OnlineCateringSystem {
                     System.out.println("=====================================");
                     System.out.println("|            Login Page             |");
                     System.out.println("=====================================");
-                    login.loginPage();
+                    usernameInputCheck = login.loginPage();
+                    if (usernameInputCheck == "exit") {
+                        usernameInputCheck = startingInterfaceMenu();
+                    }
                     break;
                 case 2:
+                    System.out.print("\n");
+                    System.out.println("=====================================");
+                    System.out.println("|           Register Page           |");
+                    System.out.println("=====================================");
+                    Register[] registerArr = new Register[20];
+                    registerUI.registerModule(registerArr);
+                    System.out.print("\n");
+                    System.out.println("=====================================");
+                    System.out.println("|            Login Page             |");
+                    System.out.println("=====================================");
+                    usernameInputCheck = login.loginPage();
+                    if (usernameInputCheck == "exit") {
+                        usernameInputCheck = startingInterfaceMenu();
+                    }
+
                     break;
                 case 3:
                     System.exit(0);
@@ -55,10 +69,51 @@ public class OnlineCateringSystem {
                     break;
             }
         } while (choice < 1 || choice > 4);
+        
+        //return username
+        return usernameInputCheck;
     }
 
     public static void main(String[] args) throws IOException, FileNotFoundException, InterruptedException {
-        startingInterfaceMenu();
+        finalUsername = startingInterfaceMenu();
+        while (finalUsername.equals("12312313") == false) {
+            if (finalUsername.equals("1") == false || finalUsername.equals("exit") == false) {
+                customerMenu();
+            }
+        }
     }
 
+    private static void customerMenu() throws IOException, FileNotFoundException, InterruptedException {
+        int choice = 0;
+        String word = "";
+        System.out.printf("\n");
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+        System.out.println("++   Welcome back " + finalUsername);
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+        do {
+            System.out.print("\n");
+            System.out.println("Customer Menu :");
+            System.out.println("=====================================");
+            System.out.println("|   1. View Profile                 |");
+            System.out.println("|   2. View Menu                    |");
+            System.out.println("|   3. Logout                       |");
+            System.out.println("=====================================");
+            System.out.print("\n");
+            System.out.print("Enter an number : ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    finalUsername = startingInterfaceMenu();
+                    break;
+                default:
+                    System.out.println("Please insert an integer between 1 to 3. Thank you.");
+                    break;
+
+            }
+        } while (choice < 1 || choice > 3);
+    }
 }
